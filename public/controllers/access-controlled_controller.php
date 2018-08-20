946e0497329e4fc44c444540c997939ba9b71322
Mathias <4262318+dermase@users.noreply.github.com>
2018-08-20 23:21:22 +0200

    Update access-controlled_controller.php
    Implementing a session timeout. Source: https://stackoverflow.com/a/1270960

--- public/controllers/access-controlled_controller.php
+++ public/controllers/access-controlled_controller.php
@@ -8,4 +8,27 @@ if(!$fgmembersite->CheckLogin())
     exit;
 }
 
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600))
+{
+	// last request was more than 10 minutes ago
+	session_unset(); // unset $_SESSION variable for the run-time
+	session_destroy(); // destroy session data in storage
+	$fgmembersite->RedirectToURL("login.php");
+	exit;
+}
+
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+if (!isset($_SESSION['CREATED']))
+{
+	$_SESSION['CREATED'] = time();
+}
+else
+if (time() - $_SESSION['CREATED'] > 600)
+{
+	// session started more than 10 minutes ago
+	session_regenerate_id(true); // change session ID for the current session and invalidate old session ID
+	$_SESSION['CREATED'] = time(); // update creation time
+}
+
 ?>
